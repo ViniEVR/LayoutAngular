c46d1496f2613e422acac48ce9e1221e1a4a3b17
Vinicius Eduardo <vinimice42@gmail.com>
2023-01-04 12:14:46 -0300

    [Adicao] Adicionando estrutura do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FasesComponent } from './components/fases/fases.component';
+import { IssuesComponent } from './components/issues/issues.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { RelatoriosComponent } from './components/relatorios/relatorios.component';
+import { CiCdComponent } from './components/ci-cd/ci-cd.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    DashboardComponent,
+    FasesComponent,
+    IssuesComponent,
+    UsuariosComponent,
+    RelatoriosComponent,
+    CiCdComponent
   ],
   imports: [
     BrowserModule,
