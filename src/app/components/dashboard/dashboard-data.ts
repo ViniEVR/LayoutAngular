1b634f0e71763814e6f920e4dbf5b358297c5ae3
Vinicius Eduardo <vinimice42@gmail.com>
2023-01-10 09:27:54 -0300

    [Adicao] Iniciando tela de dashboard

---
+++ src/app/components/dashboard/dashboard-data.ts
@@ -0,0 +1,22 @@
+export const dashboardData = [
+    {
+        icon: 'bx bx-calendar',
+        style: 'cardYellow textCardYellow',
+        label: 'Fases em andamento'
+    },
+    {
+        icon: 'bx bx-dollar',
+        style: 'cardGreen textCardGreen',
+        label: 'Earnings (Annual)'
+    },
+    {
+        icon: 'bx bx-clipboard',
+        style: 'cardBlue textCardBlue ',
+        label: 'Fases entregues'
+    },
+    {
+        icon: 'bx bx-chat',
+        style: 'cardRed textCardRed',
+        label: 'Fases Atrasadas'
+    },
+];
